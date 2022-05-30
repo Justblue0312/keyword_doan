786cd4303c6ced0fd0e08c63ca71653160668c27
Justblue0312 <trao0312@gmail.com>
2022-05-30 11:03:38 +0700

    update staticroot

--- keyword_proj/keyword_proj/settings.py
+++ keyword_proj/keyword_proj/settings.py
@@ -157,8 +157,8 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
-# MEDIA_ROOT = BASE_DIR / 'static/images'
-# STATIC_ROOT = BASE_DIR / 'staticfiles'
+MEDIA_ROOT = BASE_DIR / 'static/images'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
